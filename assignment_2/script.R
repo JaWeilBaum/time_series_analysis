60574f2bf5d59b2d459e0fa6d1e7300c76b8ad80
Leon Friedmann <leon@friedmann.eu>
2022-03-11 15:41:28 +0100

    Script update and gitignore update

--- assignment_2/script.R
+++ assignment_2/script.R
@@ -1,5 +1,5 @@
 library(forecast)
-
+setwd("workspace/time_series_analysis/assignment_2")
 arima_season_simulate = function(p, d, q, P, D, Q, 
                                  Period, years, 
                                  input) {
@@ -13,20 +13,62 @@ arima_season_simulate = function(p, d, q, P, D, Q,
   print(model)
   Y <- simulate(model, nsim=n)
   Y = ts(Y, freq=12)
-  print(Y)
+  # print(Y)
+  # par(mar=c(1,1,1,1))
   layout(matrix(c(1, 1, 2, 3), 2, 2, byrow = TRUE))
   
   max_value = max(max(abs(Y)), max(abs(values)))
   
   ts.plot(Y, main="Simulation", xlab="Year")
-  acf(Y,main="ACF", lag.max = Period * 7, xlab="Lag (Year)")
-  pacf(Y,main="PACF", lag.max = Period * 7, xlab="Lag (Year)")
-  dev.off()
+  acf(Y,main="ACF", lag.max = Period * 4, xlab="Lag (Year)")
+  pacf(Y,main="PACF", lag.max = Period * 4, xlab="Lag (Year)")
 }
 
+res = 200
+height = 15
+width = 20
+
+
+png("arima_1_0_0_x_0_0_0.png", width=width, height=height, units="cm", res=res)
+arima_season_simulate(1, 0, 0, 
+                      0, 0, 0, 
+                      12, 50,
+                      c(-.8, NA))
+dev.off()
+
+
+png("arima_0_0_0_x_1_0_0.png", width=width, height=height, units="cm", res=res)
+arima_season_simulate(0, 0, 0, 
+                      1, 0, 0, 
+                      12, 50,
+                      c(.8, NA))
+dev.off()
+
+png("arima_1_0_0_x_0_0_1.png", width=width, height=height, units="cm", res=res)
+arima_season_simulate(1, 0, 0, 
+                      0, 0, 1, 
+                      12, 50,
+                      c(-.9, .7, NA))
+dev.off()
+
+png("arima_1_0_0_x_1_0_0.png", width=width, height=height, units="cm", res=res)
 arima_season_simulate(1, 0, 0, 
                       1, 0, 0, 
-                      12, 75,
+                      12, 50,
                       c(.6, .8, NA))
+dev.off()
 
-png("arima_1_0_0_x_1_0_0.png", width=18, height=10, units="cm", res=200)
+png("arima_0_0_1_x_0_0_1.png", width=width, height=height, units="cm", res=res)
+arima_season_simulate(0, 0, 1, 
+                      0, 0, 1, 
+                      12, 50,
+                      c(-.4, .8, NA))
+dev.off()
+
+png("arima_0_0_1_x_1_0_0.png", width=width, height=height, units="cm", res=res)
+arima_season_simulate(0, 0, 1, 
+                      1, 0, 0, 
+                      12, 50,
+                      c(.4, -.7, NA))
+dev.off()
+
